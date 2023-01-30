7ab73a1d1f688665eb65fc0680d48a3680345ce3
ajewoleolumide <103895551+ajewoleolumide@users.noreply.github.com>
2023-01-30 11:04:40 +0000

    Update install_dependencies.sh
    update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 if ! [ -x "$(command -v httpd)" ]; then yum install -y httpd >&2;   exit 1; fi # install apache if not already installed
-
+sudo yum install php php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
