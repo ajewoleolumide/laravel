964cb1a0304465aaf0a39a989f4c493b5d05c77c
ajewoleolumide <103895551+ajewoleolumide@users.noreply.github.com>
2023-01-30 11:01:52 +0000

    Update install_dependencies.sh
    reverse

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 if ! [ -x "$(command -v httpd)" ]; then yum install -y httpd >&2;   exit 1; fi # install apache if not already installed
-sudo yum install -y amazon-linux-extras
-sudo yum update
-sudo amazon-linux-extras | grep php
-sudo amazon-linux-extras enable php8.1
-yum clean metadata
-yum install php-cli php-pdo php-fpm php-json php-mysqlnd php-gd
+
